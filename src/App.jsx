ab2be4cbc67ac78b59e4821af41179eda026dcab
Oleksii Butrin <bootreen@gmail.com>
2024-06-17 16:06:28 +0200

    Added sample database and card viewer

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,26 @@
-import { Fragment } from "react";
+import { useState } from "react";
 import { Card } from "./Card";
+import { characters } from "./database";
 import "./css/style.css";
 
-const App = () => (
-  <Fragment>
-    <Card title='Card 1' caption='OK' />
-    <Card title='Card 2' caption='Cancel' />
-    <Card title='Card 3' caption='Back' />
-    <Card title='Card 4' caption='Forward' />
-    <Card title='Card 5' caption='Exit' />
-  </Fragment>
-);
+const App = () => {
+  const [id, setId] = useState(0);
+  const { name, gender, age, hair, desc, img } = characters[id];
+  const onClickHandler = (direction) => {
+    if (direction === "left") setId(id > 0 ? id - 1 : characters.length - 1);
+    if (direction === "right") setId(id < characters.length - 1 ? id + 1 : 0);
+  };
+  return (
+    <Card
+      name={name}
+      gender={gender}
+      age={age}
+      hair={hair}
+      desc={desc}
+      img={img}
+      handler={onClickHandler}
+    />
+  );
+};
 
 export default App;
--- src/Card.jsx
+++ src/Card.jsx
@@ -1,8 +1,28 @@
-import { Button } from "./Button";
-
-export const Card = ({ title, caption }) => (
-  <div className='card'>
-    <h1>{title}</h1>
-    <Button caption={caption} />
-  </div>
-);
+export const Card = ({ name, gender, age, hair, desc, img, handler }) => {
+  const handlerLeft = () => handler("left");
+  const handlerRight = () => handler("right");
+  return (
+    <div className='card'>
+      <div className='header'>
+        <button onClick={handlerLeft}>◄</button>
+        <h1>{name}</h1>
+        <button onClick={handlerRight}>►</button>
+      </div>
+      <div className='info'>
+        <img src={img} alt={name} />
+        <ul>
+          <li>
+            <strong>Gender:</strong> {gender}
+          </li>
+          <li>
+            <strong>Age:</strong> {age}
+          </li>
+          <li>
+            <strong>Hair:</strong> {hair}
+          </li>
+        </ul>
+      </div>
+      <p className='desc'>{desc}</p>
+    </div>
+  );
+};
