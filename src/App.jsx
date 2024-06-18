b01ab74de6ed41a20407677d8ad95657d451e471
Oleksii Butrin <bootreen@gmail.com>
2024-06-18 14:02:47 +0200

    Finished localization, switched DOM-element binding for dropdown list

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,13 @@
 import { Fragment, useState } from "react";
 import { Card } from "./card";
+import { locale } from "./locale";
 import { characters } from "./database";
 import "./css/style.css";
 
 const App = () => {
   const [id, setId] = useState(0);
   const [language, setLanguage] = useState("en");
-  const [loginStatus, setLoginStatus] = useState("Log in");
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
   const { name, gender, age, hair, desc, img } = characters[id];
 
   const onPagingClickHandler = (direction) => {
@@ -14,23 +15,20 @@ const App = () => {
     if (direction === "right") setId(id < characters.length - 1 ? id + 1 : 0);
   };
 
-  const onLoginClickHandler = () =>
-    setLoginStatus(loginStatus === "Log in" ? "Log out" : "Log in");
+  const onLoginClickHandler = () => setIsLoggedIn(isLoggedIn ? false : true);
 
   const onLocaleChangeHandler = (lang) => setLanguage(lang);
 
   return (
     <Fragment>
-      <h1>South Park Characters DB</h1>
+      <h1>{locale.title[language]}</h1>
       <div className='options'>
         <div className='locale'>
-          <label htmlFor='lang'>Lang:</label>
+          <label htmlFor='lang'>{locale.lang[language]}</label>
           <select
             id='lang'
             name='locale'
-            onChange={() =>
-              onLocaleChangeHandler(document.getElementById("lang").value)
-            }
+            onChange={(event) => onLocaleChangeHandler(event.target.value)}
           >
             <option value='en'>EN</option>
             <option value='de'>DE</option>
@@ -38,9 +36,11 @@ const App = () => {
         </div>
         <div className='login'>
           <div className='avatar'>
-            {loginStatus === "Log out" && <img src='./boot-ava.jpg' alt='' />}
+            {isLoggedIn && <img src='./boot-ava.jpg' alt='' />}
           </div>
-          <button onClick={onLoginClickHandler}>{loginStatus}</button>
+          <button onClick={onLoginClickHandler}>
+            {isLoggedIn ? locale.logout[language] : locale.login[language]}
+          </button>
         </div>
       </div>
       <Card
